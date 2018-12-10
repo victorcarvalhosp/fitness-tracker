2d49e6e037b1e0749e1d75f8ae72cecbe11d7b52
Victor Fernandes <victorcarvalhosp@gmail.com>
2018-12-10 10:34:24 -0200

    Added basic routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
-import { TrainingComponent } from './auth/training/training.component';
 import { CurrentComponent } from './training/current/current.component';
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import {TrainingComponent} from './training/training.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { PastTrainingsComponent } from './training/past-trainings/past-trainings
     TrainingComponent,
     CurrentComponent,
     NewTrainingComponent,
-    PastTrainingsComponent
+    PastTrainingsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
