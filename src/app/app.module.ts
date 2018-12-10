f11a05d86b0d920c28ce7b934d814865a3d48ef3
Victor Fernandes <victorcarvalhosp@gmail.com>
2018-12-10 10:30:32 -0200

    Basic project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './auth/training/training.component';
+import { CurrentComponent } from './training/current/current.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent
   ],
   imports: [
     BrowserModule,
