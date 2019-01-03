b2173231446a6433d16b04412d0fff45bd42291b
Victor Fernandes <victorcarvalhosp@gmail.com>
2019-01-03 10:18:22 -0200

    Changed training module to use redux architecture

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,32 +1,33 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {NgForm} from '@angular/forms';
-import {Observable, Subscription} from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
-import {TrainingService} from '../training.service';
-import {Exercise} from '../exercise.model';
+import { TrainingService } from '../training.service';
+import { Exercise } from '../exercise.model';
+import { UIService } from '../../shared/ui.service';
+import * as fromTraining from '../training.reducer';
 import * as fromRoot from '../../app.reducer';
-import {Store} from '@ngrx/store';
-
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.css']
 })
-export class NewTrainingComponent implements OnInit, OnDestroy {
-  exercises: Exercise[];
+export class NewTrainingComponent implements OnInit {
+  exercises$: Observable<Exercise[]>;
   isLoading$: Observable<boolean>;
-  private exerciseSubscription: Subscription;
 
-  constructor(private trainingService: TrainingService, private store: Store<fromRoot.State>) {}
+  constructor(
+    private trainingService: TrainingService,
+    private uiService: UIService,
+    private store: Store<fromTraining.State>
+  ) {}
 
   ngOnInit() {
     this.isLoading$ = this.store.select(fromRoot.getIsLoading);
-    this.exerciseSubscription = this.trainingService.exercisesChanged.subscribe(
-      exercises => {
-        this.exercises = exercises;
-      }
-    );
+    this.exercises$ = this.store.select(fromTraining.getAvailableExercises);
     this.fetchExercises();
   }
 
@@ -36,11 +37,5 @@ export class NewTrainingComponent implements OnInit, OnDestroy {
 
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise);
-  }
-
-  ngOnDestroy() {
-    if (this.exerciseSubscription) {
-      this.exerciseSubscription.unsubscribe();
-    }
   }
 }
